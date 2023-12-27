63d67e88c2a698cd41cd82eb0984042954836afd
btLong402 <111285054+btLong402@users.noreply.github.com>
2023-12-27 13:28:48 +0700

    fix get-courses

--- msd_elearning_website/actions/get-chapter.ts
+++ msd_elearning_website/actions/get-chapter.ts
@@ -1,4 +1,5 @@
 import { db } from "@/lib/db";
+import { Attachment, Chapter } from "@prisma/client";
 
 interface GetChapterProps {
   userId: string;
@@ -42,6 +43,39 @@ export const getChapter = async ({
       throw new Error("Chapter or course not found");
     }
 
+    let muxData = null;
+    let attachments: Attachment[] = [];
+    let nextChapter: Chapter | null = null;
+
+    if (purchase) {
+      attachments = await db.attachment.findMany({
+        where: {
+          courseId: courseId,
+        },
+      });
+    }
+
+    if (chapter.isFree || purchase) {
+      muxData = await db.muxData.findUnique({
+        where: {
+          chapterId: chapterId,
+        },
+      });
+
+      nextChapter = await db.chapter.findFirst({
+        where: {
+          courseId: courseId,
+          isPublished: true,
+          position: {
+            gt: chapter?.position,
+          },
+        },
+        orderBy: {
+          position: "asc",
+        },
+      });
+    }
+
     const userProgress = await db.userProgress.findUnique({
       where: {
         userId_chapterId: {
@@ -54,6 +88,9 @@ export const getChapter = async ({
     return {
       chapter,
       course,
+      muxData,
+      attachments,
+      nextChapter,
       userProgress,
       purchase,
     };

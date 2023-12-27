5d1202dc5aaf534a3593585cdb79e4fea92140ed
btLong402 <111285054+btLong402@users.noreply.github.com>
2023-12-27 13:26:44 +0700

    chappter page

--- msd_elearning_website/actions/get-chapter.ts
+++ msd_elearning_website/actions/get-chapter.ts
@@ -1,10 +1,11 @@
 import { db } from "@/lib/db";
+import { Attachment, Chapter } from "@prisma/client";
 
 interface GetChapterProps {
   userId: string;
   courseId: string;
   chapterId: string;
-};
+}
 
 export const getChapter = async ({
   userId,
@@ -17,8 +18,8 @@ export const getChapter = async ({
         userId_courseId: {
           userId,
           courseId,
-        }
-      }
+        },
+      },
     });
 
     const course = await db.course.findUnique({
@@ -28,18 +29,51 @@ export const getChapter = async ({
       },
       select: {
         price: true,
-      }
+      },
     });
 
     const chapter = await db.chapter.findUnique({
       where: {
         id: chapterId,
         isPublished: true,
-      }
+      },
     });
 
     if (!chapter || !course) {
       throw new Error("Chapter or course not found");
+    }
+
+    let muxData = null;
+    let attachments: Attachment[] = [];
+    let nextChapter: Chapter | null = null;
+
+    if (purchase) {
+      attachments = await db.attachment.findMany({
+        where: {
+          courseId: courseId,
+        },
+      });
+    }
+
+    if (chapter.isFree || purchase) {
+      muxData = await db.muxData.findUnique({
+        where: {
+          chapterId: chapterId,
+        },
+      });
+
+      nextChapter = await db.chapter.findFirst({
+        where: {
+          courseId: courseId,
+          isPublished: true,
+          position: {
+            gt: chapter?.position,
+          },
+        },
+        orderBy: {
+          position: "asc",
+        },
+      });
     }
 
     const userProgress = await db.userProgress.findUnique({
@@ -47,13 +81,16 @@ export const getChapter = async ({
         userId_chapterId: {
           userId,
           chapterId,
-        }
-      }
+        },
+      },
     });
 
     return {
       chapter,
       course,
+      muxData,
+      attachments,
+      nextChapter,
       userProgress,
       purchase,
     };
@@ -61,6 +98,12 @@ export const getChapter = async ({
     console.log("[GET_CHAPTER]", error);
     return {
       chapter: null,
-    }
+      course: null,
+      muxData: null,
+      attachments: [],
+      nextChapter: null,
+      userProgress: null,
+      purchase: null,
+    };
   }
-}
+};
